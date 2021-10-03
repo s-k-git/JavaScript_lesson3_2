372bca6cbb49f06964ef5b3c12f321e5ab20ab47
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-10-04 01:04:45 +0900

    JavaScript課題③-2のレビュー依頼

--- js/lesson3_2.js
+++ js/lesson3_2.js
@@ -1,13 +1,17 @@
+"use strict";
+
 const todos = [];
+const comment = document.getElementById("comment");
+const table = document.getElementById("table-contents");
 
 function addtable() {
-    const comment = document.getElementById("comment");
     const todo = { task: comment.value, status: "作業中", deletion: "削除" }
     todos.push(todo);
+    listdescription();
+}
 
-    let table = document.getElementById("table-contents");
+function listdescription() {
     table.innerHTML = "";
- 
     todos.forEach(function (value, index) {
         const statusbtn = document.createElement("input");
         statusbtn.type = "button";
@@ -16,6 +20,10 @@ function addtable() {
         deletebtn.type = "button";
         deletebtn.value = value.deletion;
         deletebtn.id = "deleteNo" + index;
+        //deletebtn.addEventListener('click', () => {
+        //    todos.splice(index, 1);
+        //    listdescription();
+        //});
 
         let newRow = table.insertRow();
         let newCell = newRow.insertCell();
@@ -32,37 +40,12 @@ function addtable() {
         newCell = newRow.insertCell();
         newCell.appendChild(deletebtn);
 
-        document.getElementById(`deleteNo${index}`).onclick = function deletetable() {
+        //idを取得するのでdeletebtnをappend後でないとnullエラー ※getElementはhtmlから取得するのでdeletebtn.idを指定済でもnull
+        document.getElementById(`deleteNo${index}`).onclick = function () {
             todos.splice(index, 1);
-        
-            let table = document.getElementById("table-contents");
-            table.innerHTML = "";        
-        
-            todos.forEach(function (value, index) {
-                const statusbtn = document.createElement("input");
-                statusbtn.type = "button";
-                statusbtn.value = value.status;
-                const deletebtn = document.createElement("input");
-                deletebtn.type = "button";
-                deletebtn.value = value.deletion;
-                deletebtn.id = "deleteNo" + index;
-        
-                let newRow = table.insertRow();
-                let newCell = newRow.insertCell();
-                let newText = document.createTextNode(index);
-                newCell.appendChild(newText);
-        
-                newCell = newRow.insertCell();
-                newText = document.createTextNode(value.task);
-                newCell.appendChild(newText);
-        
-                newCell = newRow.insertCell();
-                newCell.appendChild(statusbtn);
-        
-                newCell = newRow.insertCell();
-                newCell.appendChild(deletebtn);
-            });
+            listdescription();
         }
     });
     comment.value = "";
+    //最初の宣言をconst comment = document.getElementById("comment").value;にすると再定義エラーがでるので全体を取得し.valueで都度出力
 }
